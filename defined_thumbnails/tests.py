46036494527f2c0811402dbb646200abfc18b2bd
Jj <jjdelc@gmail.com>
2012-11-03 18:31:42 -0500

    tests skeleton

---
+++ defined_thumbnails/tests.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from django.test import TestCase
+
+from defined_thumbnails import helpers
+from defined_thumbnails.templatetags import thumbnail
+
+
+class TestHelpers(TestCase):
+    def test_is_enabled(self):
+        pass
+
+    def test_is_valid_geometry(self):
+        pass
+
+    def test_get_named_sizes(self):
+        pass
+
+    def test_get_defined_sizes(self):
+        pass
+
+    def test_convert_to_geometry(self):
+        pass
+
+
+class TestTemplateTag(TestCase):
+    def test_init(self):
+        pass
+
+    def test_ttag(self):
+        pass
