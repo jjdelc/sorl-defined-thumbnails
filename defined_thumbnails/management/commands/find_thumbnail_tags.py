1a34682a6081fe90fbc83fc2aeebbd0cacf75c1a
Jj <jjdelc@gmail.com>
2012-11-04 18:46:44 -0500

    find tags command

--- defined_thumbnails/management/commands/find_thumbnail_tags.py
+++ defined_thumbnails/management/commands/find_thumbnail_tags.py
@@ -1,6 +1,14 @@
 # coding: utf-8
 
+import os
+import re
+
+from django.conf import settings
+from django.template.loaders import app_directories
 from django.core.management.base import BaseCommand
+
+
+TTAG_RE = re.compile('{%\ *thumbnail')
 
 
 def make_line_data(num, line, filename):
@@ -12,16 +20,31 @@ def make_line_data(num, line, filename):
 
 
 def has_thumbnail_tag(line):
-    return 'thumbnail' in line
+    return TTAG_RE.match(line)
 
 
 def tlines_in_file(template_file):
     lines = []
-    for num, line in open(template_file):
+    for num, line in enumerate(open(template_file), 1):
         if has_thumbnail_tag(line):
             lines.append(make_line_data(num, line, template_file))
 
     return lines
+
+
+def template_files():
+    """
+    Returns a list of all template files full path
+    """
+    complete_files = []
+    all_dirs = list(settings.TEMPLATE_DIRS) + list(app_directories.app_template_dirs)
+    for tdir in all_dirs:
+        for path, dirs, files in os.walk(tdir):
+            for fname in files:
+                complete_files.append(os.path.realpath(
+                    os.path.join(path, fname)))
+
+    return complete_files
 
 
 def thumbnail_lines():
@@ -33,6 +56,13 @@ def thumbnail_lines():
     return tlines
 
 
+def convert_to_txt(tline):
+    return u'%s:%s - %s' % (tline['file'], tline['number'], tline['line'])
+
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        print '\n'.join(thumbnail_lines())
+        out = []
+        for tline in thumbnail_lines():
+            out.append(convert_to_txt(tline).encode('utf-8'))
+        print u'\n'.join(out)
